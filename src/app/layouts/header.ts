458b8e9491a7e261c3cf05e327adf9e96f65cc1d
NIRMAL <nirmaltrejilal@gmail.com>
2024-08-23 15:07:46 +0530

    repeated notifications shown bug fix

--- src/app/layouts/header.ts
+++ src/app/layouts/header.ts
@@ -16,7 +16,6 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { environment } from 'src/environments/environment';
 
-
 @Component({
     moduleId: module.id,
     selector: 'header',
@@ -29,22 +28,23 @@ import { environment } from 'src/environments/environment';
     ],
 })
 export class HeaderComponent {
-
-   
     store: any;
     msg: any;
     search = false;
     use_det: any;
-    us_id:any;
-    user_name :any;
-    user_email :any;
-    messages:any;
-    notification:any;
-    notcount:any;
-    chat='Chat';
-    service='Service';
-    calender='Calender'
-    languages=[{'id':1,'name':'English','code':'en'},{'id':2,'name':'Arabic','code':'ae'}];
+    us_id: any;
+    user_name: any;
+    user_email: any;
+    messages: any;
+    notification: any;
+    notcount: any;
+    chat = 'Chat';
+    service = 'Service';
+    calender = 'Calender';
+    languages = [
+        { id: 1, name: 'English', code: 'en' },
+        { id: 2, name: 'Arabic', code: 'ae' },
+    ];
 
     defaultParams = {
         id: null,
@@ -66,78 +66,57 @@ export class HeaderComponent {
         public router: Router,
         private appSetting: AppService,
         private sanitizer: DomSanitizer,
-        private systemService:SystemService,
+        private systemService: SystemService,
         private fb: FormBuilder
     ) {
-
-        
-  
-        this.user_name = localStorage.getItem("us_firstname");
-        this.user_name = this.user_name?atob(atob(this.user_name)):"";
-        this.user_email = localStorage.getItem("us_email");
-        this.user_email = this.user_email?atob(atob(this.user_email)):"";
+        this.user_name = localStorage.getItem('us_firstname');
+        this.user_name = this.user_name ? atob(atob(this.user_name)) : '';
+        this.user_email = localStorage.getItem('us_email');
+        this.user_email = this.user_email ? atob(atob(this.user_email)) : '';
         this.systemService.get_userdet().subscribe((rdata: any) => {
             if (rdata.ret_data == 'success') {
                 //this.use_det=rdata.us_data
-                this.us_id=rdata.us_data.us_id
+                this.us_id = rdata.us_data.us_id;
             }
-          });
-          this.usr_notification();
-          
-          
+        });
+        this.usr_notification();
     }
 
-    usr_notification(){
-
-        
-        
+    usr_notification() {
         this.systemService.get_notification().subscribe((rdata: any) => {
-            if (rdata.ret_data == 'success') { 
-                
-                
-                this.notification=rdata.notifications;
-                this.notcount=rdata.notif_len
+            if (rdata.ret_data == 'success') {
+                this.notification = rdata.notifications;
+                this.notcount = rdata.notif_len;
 
-                if((this.notification.length)>0){
-                    this.notification_alert(this.notification[0]['nt_content'])
-                    
-                    
+                console.log(' this.notification', this.notification);
+                if (this.notification.length > 0 && this.notification[0]['nt_deleteflag'] == '0') {
+                    this.notification_alert(this.notification[0]['nt_content']);
                 }
-                
-               
-                
+            } else if (rdata.ret_data == 'fail') {
+                this.notification = [];
+                this.notcount = 0;
             }
-            else if(rdata.ret_data == 'fail'){
-                
-                this.notification=[];
-                this.notcount=0;    
-            }
-          });
+        });
     }
 
-    clear_all_notif(){
-        let data={
-            notif_data:this.notification
-        } 
+    clear_all_notif() {
+        let data = {
+            notif_data: this.notification,
+        };
 
         this.systemService.clear_us_notif(data).subscribe((rdata: any) => {
             if (rdata.ret_data == 'success') {
                 this.notification = [];
-                
-            }else{}
-          }); 
+            } else {
+            }
+        });
     }
 
-    hard_refresh()
-    {
+    hard_refresh() {
         window.location.reload();
-
     }
- 
 
- 
     ngOnInit() {
-        
         this.setActiveDropdown();
         this.router.events.subscribe((event) => {
             if (event instanceof NavigationEnd) {
@@ -146,36 +125,39 @@ export class HeaderComponent {
         });
     }
     removeMessage(msgId: any) {
-        
-        
-        let data={
-            nt_id:msgId
-        } 
+        let data = {
+            nt_id: msgId,
+        };
 
         this.systemService.updatenotification(data).subscribe((rdata: any) => {
             if (rdata.ret_data == 'success') {
-                
-                
-               
                 this.notification = this.notification.filter((msg: any) => {
-
                     return msg.nt_id != rdata.nt_id;
                 });
-                
-                
-                
             }
-          });
-    }
-    
-    removeNotification(id:any){
-
+        });
     }
 
-    chatrouter(){
+    deleteMessage(msgId: any) {
+        let data = {
+            nt_id: msgId,
+        };
+
+        this.systemService.deletenotification(data).subscribe((rdata: any) => {
+            if (rdata.ret_data == 'success') {
+                this.notification = this.notification.filter((msg: any) => {
+                    return msg.nt_id != rdata.nt_id;
+                });
+            }
+        });
+    }
+
+    removeNotification(id: any) {}
+
+    chatrouter() {
         this.router.navigateByUrl('chat-home');
     }
-    create_Service_Req(){
+    create_Service_Req() {
         this.router.navigateByUrl('service-request-create');
     }
     setActiveDropdown() {
@@ -199,36 +181,25 @@ export class HeaderComponent {
         }
     }
 
-    
-
     changeLanguage(item: any) {
-        
-        
         if (item.id == 2) {
             localStorage.setItem('rtlClass', 'rtl');
-            localStorage.setItem('i18n_locale', 'ae'); 
-        }else{
+            localStorage.setItem('i18n_locale', 'ae');
+        } else {
             localStorage.setItem('rtlClass', 'ltr');
             localStorage.setItem('i18n_locale', 'en');
         }
-        window.location.reload()
-        
+        window.location.reload();
     }
 
-    logout(){
-       
+    logout() {
         this.systemService.logout().subscribe((rdata: any) => {
             if (rdata.ret_data == 'success') {
                 localStorage.clear();
                 this.router.navigateByUrl('/');
             }
-          });
-        
+        });
     }
-
-
-
-  
 
     showMessage(msg = '', type = 'success') {
         const toast: any = Swal.mixin({
@@ -245,8 +216,7 @@ export class HeaderComponent {
         });
     }
 
-
-    show_cust_alert(){
+    show_cust_alert() {
         Swal.fire({
             icon: 'warning',
             title: 'Are you sure?',
@@ -257,19 +227,22 @@ export class HeaderComponent {
             customClass: 'sweet-alerts',
         }).then((result) => {
             if (result.value) {
-                this.clear_all_notif()
-                this.showMessage("Notifications cleared",'success')
+                this.clear_all_notif();
+                this.showMessage('Notifications cleared', 'success');
             }
         });
     }
 
-    notification_alert(msg :any) {
-        const position = 'bottom-end', showCloseButton = true, duration = 3000,color='info'
+    notification_alert(msg: any) {
+        const position = 'bottom-end',
+            showCloseButton = true,
+            duration = 3000,
+            color = 'info';
         const toast = Swal.mixin({
             toast: true,
             position: <any>(position || 'bottom-start'),
             showConfirmButton: false,
-             timer: duration,
+            timer: duration,
             showCloseButton: showCloseButton,
             customClass: {
                 popup: `color-${color}`,
@@ -279,8 +252,7 @@ export class HeaderComponent {
         toast.fire({
             title: msg,
         });
+
+        this.deleteMessage(this.notification[0]['nt_id']);
     }
-
-
-    
 }
